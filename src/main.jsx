93f7587e84fc7043de610c49dc4c21b9bf0d6e46
TangTina738 <Tangtina738@gmail.com>
2025-12-29 03:43:33 -0500

    Fixed Header.css import capital

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './style/index.css'
-import './Style/Header.css'
-import './Style/Home.css';
+import './style/Header.css'
+import './style/Home.css';
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
